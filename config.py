97c84cb1aee918007678eb51f5fed5e10beecb5e
EnejiStats <enejistats@gmail.com>
2025-06-07 18:15:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+import os
+class Config: SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key") SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///soccer.db") SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class ProdConfig(Config): DEBUG = False
+
+class DevConfig(Config): DEBUG = True
