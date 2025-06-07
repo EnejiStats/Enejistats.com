97d10b2a6f7276332b022e142ee4a65b5e466b96
EnejiStats <enejistats@gmail.com>
2025-06-07 17:57:18 +0100

    Update route.py

--- app/route.py
+++ app/route.py
@@ -1,11 +1,1 @@
-from flask import Blueprint, render_template
 
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    return render_template('index.html')
-
-@main.route('/players')
-def players():
-    return render_template('players.html')
